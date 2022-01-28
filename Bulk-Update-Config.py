c9ca812d29db9632478c785bf3cff29a6e3068dd
A-Basra <82539141+A-Basra@users.noreply.github.com>
2022-01-28 14:29:29 +0000

    GUI Working

--- Bulk-Update-Config.py
+++ Bulk-Update-Config.py
@@ -1,14 +1,12 @@
 #Version 2.0.0 GUI 
 #By Aman Basra
-
+from tkinter import *
 import tkinter as TK
+from tkinter import scrolledtext
 from tkinter import filedialog
 import meraki
 import pandas as pd
 import time
-
-path = None
-orgID = None
 
 def browse():
     path = filedialog.askdirectory()
@@ -19,13 +17,35 @@ def getOrgID():
     dashboard = meraki.DashboardAPI(API_KEY)
     response = dashboard.organizations.getOrganizations()
     orgidlbl.config(text=response)
-    return response
+    output = []
+    for l in response:
+        for x in l.values():
+            output.append(x)
+    orgID = output[0]
+    orgName = output[1]
+    Output.insert(TK.INSERT, orgName + "\n" + orgID + "\n")
+    return orgID, orgName
+
+def getNetworkID():
+    orgID = getOrgID()
+    API_KEY = getAPI.get(1.0, 'end-1c')
+    dashboard = meraki.DashboardAPI(API_KEY)
+    response = dashboard.organizations.getOrganizationNetworks(orgID[0])
+    data = []
+    for l in response:
+        for x in l.values():
+            data.append(x)
+    NetworkID = data[0]
+    NetworkName = data[2]
+    Output.insert(TK.INSERT, NetworkName + "\n"+ NetworkID + "\n")
+    return NetworkID, NetworkName
 
 def update():
+    netID = getNetworkID()
     # API Setup
     API_KEY = getAPI.get(1.0, 'end-1c')
     dashboard = meraki.DashboardAPI(API_KEY)
-    network_id = '<NETWORK-ID>'
+    network_id = netID[0]
 
 
     # Import CSV
@@ -61,24 +81,26 @@ def update():
         except:
             continue
 
+
+
 #Create GUI Window
 frame = TK.Tk()
 frame.title("Meraki Bulk Update Tool")
-frame.geometry('500x500')
+frame.geometry('1000x500')
 
-APIlbl  = TK.Label(frame, text="API Key", anchor= "w", pady=5)
+APIlbl  = TK.Label(frame, text="API Key", pady=5)
 getAPI = TK.Text(frame,
                     height= 1,
                     width = 50,)
 
-CSVPathlbl  = TK.Label(frame, text="CSV", anchor= "w", pady=5)
-printCSVPathlbl  = TK.Label(frame, text=path, anchor= "w", pady=5)
-orgidlbl  = TK.Label(frame, text="", anchor= "w", pady=5)
+CSVPathlbl  = TK.Label(frame, text="CSV", pady=5)
+printCSVPathlbl  = TK.Label(frame, text="", pady=5)
+orgidlbl  = TK.Label(frame, text="", pady=5)
 
 browseButton =TK.Button(frame, text='Browse', command=browse)
+submitButton =TK.Button(frame, text='Submit', command=getNetworkID)
 
-submitButton =TK.Button(frame, text='Submit', command=getOrgID)
-
+Output = scrolledtext.ScrolledText(frame, height='20', width='100', wrap="word")
 
 APIlbl.pack()
 getAPI.pack()
@@ -87,6 +109,8 @@ printCSVPathlbl.pack()
 orgidlbl.pack()
 browseButton.pack()
 submitButton.pack()
+
+Output.pack()
 
 frame.mainloop()
 
